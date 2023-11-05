5fd7fc1997e1a96259c4e4da42784ebacdb3de2d
Vicente Pareja <vicente.pareja@uc.cl>
2023-11-05 13:42:10 -0300

    corrección tildes y que no hayan clicks.

--- botwhatsap.py
+++ botwhatsap.py
@@ -1,47 +1,59 @@
 import time
 import pandas as pd
 import pyautogui
+import pyperclip
+from parametros import *
 
-# Constants
-EXCEL_FILE = "whatsapp_numbers.xlsx"
-PHONE_COLUMN = 'Numero de telefono'
+def send_message(phone_number, name, message_template):
+    # Formatear el mensaje con el nombre del destinatario
+    message = message_template.format(name=name)
+    
+    # Copiar el mensaje al portapapeles
+    pyperclip.copy(message)
 
-MESSAGE = """*Hola! Me llamo Vicente y este menaje fué enviado por un bot!*"""
-
-# Read the phone numbers from the Excel file
-def read_phone_numbers(filename, column):
-    df = pd.read_excel(filename)
-    return df[column].tolist()
-
-# Send a message through WhatsApp Web
-def send_message(phone_number, message):
-    # Open a new browser tab
+    # Abrir una nueva pestaña del navegador
     pyautogui.hotkey('ctrl', 't')
 
-    # Open WhatsApp Web with the specified phone number
+    # Abrir WhatsApp Web con el número de teléfono especificado
     pyautogui.typewrite(f'https://web.whatsapp.com/send?phone={phone_number}')
     pyautogui.press('enter')
 
-    # Wait for the page to load
+    # Esperar a que se cargue la página
     time.sleep(10)
 
-    # Type and send the message
-    for line in message.split('\n'):
-        pyautogui.typewrite(line)
-        pyautogui.press('enter')
+    # Simular un clic para asegurarse de que el cursor esté en el área de texto
+    # Los valores (x, y) deberán ser ajustados según la resolución y la disposición de tu pantalla
+    pyautogui.click(x=1000, y=950) 
 
-    # Close the tab
+    # Esperar un poco después del clic
+    time.sleep(1)
+
+    # Pegar el mensaje desde el portapapeles
+    pyautogui.hotkey('ctrl', 'v')
+
+    # Esperar un momento para que el mensaje se pegue correctamente
+    time.sleep(1)
+
+    # Enviar el mensaje
+    pyautogui.press('enter')
+
+    # Cerrar la pestaña
     time.sleep(2)
     pyautogui.hotkey('ctrl', 'w')
 
-def main():
-    # Initialize
-    time.sleep(3)
-    phone_numbers = read_phone_numbers(EXCEL_FILE, PHONE_COLUMN)
+def read_contacts(filename, name_column, phone_column):
+    df = pd.read_excel(filename)
+    print("Dataframe cargado")
+    return df[name_column].tolist(), df[phone_column].tolist()
 
-    # Send the message to each number in the list
-    for i, phone_number in enumerate(phone_numbers, 1):
-        send_message(int(phone_number), MESSAGE)
+def main():
+    # Inicialización
+    time.sleep(3)
+    names, phone_numbers = read_contacts(EXCEL_FILE, NAME_COLUMN, PHONE_COLUMN)
+
+    # Enviar el mensaje a cada contacto en la lista
+    for i, (name, phone_number) in enumerate(zip(names, phone_numbers), 1):
+        send_message(int(phone_number), name, MESSAGE_TEMPLATE)
         print(f"Mensajes enviados: {i}")
 
 if __name__ == "__main__":
---
+++ parametros.py
@@ -0,0 +1,6 @@
+EXCEL_FILE = "gente.xlsx"
+NAME_COLUMN = "Nombre"
+PHONE_COLUMN = "Numero"
+
+# Message template
+MESSAGE_TEMPLATE = """Hola {name}! Me llamo Vicente y este mensaje fué enviado por un bot! Aguante *Potenciaaaa*!"""
